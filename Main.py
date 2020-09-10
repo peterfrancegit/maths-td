5fb9cb1830800e44da22bc5bd027ebd55dec7569
eddie9802 <ehamilton9810@gmail.com>
2020-09-10 20:15:30 +0100

    Main.py is the file to run the program from

---
+++ Main.py
@@ -0,0 +1,42 @@
+import pygame
+import grid
+
+WIDTH = None
+HEIGHT = None
+GAME_DISPLAY = None
+
+
+# Initialises pygame and sets up a window
+def _initialise_pygame():
+    global WIDTH
+    global HEIGHT
+    global GAME_DISPLAY
+
+    pygame.init()
+    pygame.display.set_caption('Maths TD')
+
+    # Gets the resolution of the display that's running the game
+    infoObject = pygame.display.Info()
+    WIDTH, HEIGHT = infoObject.current_w, infoObject.current_h
+    GAME_DISPLAY = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+
+
+def _game_loop():
+    gridSize = 10
+    graph = grid.make_grid(gridSize, gridSize, [])
+    print(graph)
+    clock = pygame.time.Clock()
+
+    hasQuit = False
+    while not hasQuit:
+        pygame.display.update()
+        for event in pygame.event.get() + [pygame.event.wait()]:
+            if event.type == pygame.QUIT:
+                hasQuit = True
+            else:
+                pass
+        clock.tick(30)
+
+if __name__ == "__main__":
+    _initialise_pygame()
+    _game_loop()

57612f66c388235324416e6edca2ca40349a3012
eddie9802 <ehamilton9810@gmail.com>
2020-09-22 20:47:28 +0100

    added test_initialise_exit_when_square_grid_not_initialised test

--- unittests/Grid_Tests.py
+++ unittests/Grid_Tests.py
@@ -1,10 +1,12 @@
 import unittest
 from unittest.mock import Mock
+from io import StringIO
 import sys
 
 sys.path.insert(1, '../src')
 import pygame
 from Grid import Grid
+from Square import Square
 from Square import Exit
 
 class GridTests(unittest.TestCase):
@@ -25,16 +27,31 @@ class GridTests(unittest.TestCase):
         """Tests if the initialise_square_grid function initialises the square_grid variable in the given grid object correctly"""
         
         # Sets the return value for the mock objects .get_size() method return
-        self.display.get_size.return_value = (500, 500)
-        
+
+        screenWidth, screenHeight = (500, 500)
+        self.display.get_size.return_value = (screenWidth, screenHeight)
         self.grid.initialise_square_grid(self.display)
         self.grid.initialise_exit()
 
     
         self.assertTrue(isinstance(self.grid.square_grid[self.grid.exit_square[0]][self.grid.exit_square[1]], Exit))
 
-    
+    def test_initialise_exit_when_square_grid_not_initialised(self):
+        """Tests if initialise_exit prints the correct message when the grid instance variable square_grid has not been initialised"""
+        
+        # Sets the return value for function get_size
+        screenWidth, screenHeight = (500, 500)
+        self.display.get_size.return_value = (screenWidth, screenHeight)
 
+        stdout = StringIO()
+        sys.stdout = stdout
+
+        #self.grid.initialise_square_grid(self.display)
+        self.grid.initialise_exit()
+
+        sys.stdout = sys.__stdout__
+        self.assertEqual("Instance variable square_grid has not been intialised properly.  Please call the method initialise_square_grid to initialise it.\n", 
+        stdout.getvalue())
 
 
 if __name__ == '__main__':

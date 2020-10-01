2c6ba5bcf4c3f0e66c1575633c1b51391ca1c876
peterfrancegit <peter-france@hotmail.co.uk>
2020-10-01 21:30:16 +0100

    upgrading costs player

--- src/Grid.py
+++ src/Grid.py
@@ -115,6 +115,7 @@ class Grid:
     def upgrade_tower(self, tower):
         """Increases the level of a Tower"""
         tower.upgrade()
+        self.souls -= max(1, tower.level * tower.cost // 3)
 
     def spawn_numemy(self, start_val, coins, speed, weight):
         """Adds a new Numemy object to the square_grid spawner_square"""
